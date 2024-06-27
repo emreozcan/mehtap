ed76277072f61821eb91e3b175814121a8f92bff
Emre Özcan <emre@emreis.com>
2024-06-28 00:58:06 +0300

    Add getting variable value

--- ay/__init__.py
+++ ay/__init__.py
@@ -14,7 +14,7 @@ lua_parser = lark.Lark(
 )
 
 text = """
-x = 2 + 3 * 9
+x, y = 3, x + 1
 """
 
 print("\n".join([f"> {l}" for l in text[1:].splitlines()]))
@@ -25,22 +25,80 @@ parsed_lua = lua_parser.parse(text)
 
 Env = namedtuple("Env", ["glob", "loc"])
 
+class LuaValue():
+    def __init__(self) -> None:
+        pass
+
+class LuaNil(LuaValue):
+    def __init__(self) -> None:
+        super().__init__()
+
+    def __str__(self) -> str:
+        return "nil"
+
+    def __repr__(self) -> str:
+        return str(self)
+
+class LuaInt64(LuaValue):
+    __slots__ = ["value"]
+
+    def __init__(self, value) -> None:
+        super().__init__()
+        self.value = value
+
+    def __str__(self) -> str:
+        return str(self.value)
+
+    def __repr__(self) -> str:
+        return f"LuaInt64({self.value})"
+
+class LuaDouble(LuaValue):
+    __slots__ = ["value"]
+
+    def __init__(self, value) -> None:
+        super().__init__()
+        self.value = value
+
+    def __str__(self) -> str:
+        return str(self.value)
+
+    def __repr__(self) -> str:
+        return f"LuaDouble({self.value})"
+
 class BlockInterpreter(lark.visitors.Interpreter):
     def __init__(self, env) -> None:
         super().__init__()
         self.env = env
 
     def stat_assignment(self, tree):
-        var_list = self.visit(tree.children[0])
-        exp_list = self.visit(tree.children[1])
-        for var, exp in zip(var_list, exp_list):
-            self.env.loc[var] = exp
+        var_list = tree.children[0].children
+        exp_list = tree.children[1].children
+        exp_vals = [self.visit(exp) for exp in exp_list]
+        for var, exp_val in zip(var_list, exp_vals):
+            var_name = str(var.children[0].children[0])
+            if var_name in self.env.loc:
+                self.env.loc[var_name] = exp_val
+            else:
+                self.env.glob[var_name] = exp_val
 
     def name(self, tree):
         return str(tree.children[0])
 
     def numeral(self, tree):
-        return int(tree.children[0])
+        literal = tree.children[0]
+        return int(literal)  # TODO.
+
+    def prefixexp(self, tree):
+        return self.visit(tree.children[0])
+
+    def var_name(self, tree):
+        name = self.visit(tree.children[0])
+        if name in self.env.loc:
+            return self.env.loc[name]
+        elif name in self.env.glob:
+            return self.env.glob[name]
+        else:
+            return LuaNil()
 
     def exp_sum(self, tree):
         left = self.visit(tree.children[0])
@@ -52,7 +110,7 @@ class BlockInterpreter(lark.visitors.Interpreter):
             return left - right
         else:
             raise ValueError(f"Unknown sum operator: {op}")
-    
+
     def exp_product(self, tree):
         left = self.visit(tree.children[0])
         op = tree.children[1].children[0]
