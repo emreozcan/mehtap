6821d395be05f1aba2709e47e52686471eb8c896
Emre Özcan <emre@emreis.com>
2024-07-05 02:35:53 +0300

    Ignore single line comments

--- ay/__main__.py
+++ ay/__main__.py
@@ -3,18 +3,18 @@ from . import lua_parser, LuaInterpreter
 
 def main():
     text = """
-     x = 10                -- global variable
-     do                    -- new block
-       local x = x         -- new 'x', with value 10
-       print(x)            --> 10
-       x = x+1
-       do                  -- another block
-         local x = x+1     -- another 'x'
-         print(x)          --> 12
-       end
-       print(x)            --> 11
-     end
-     print(x)              --> 10  (the global one)
+x = 10                    -- global variable
+do                        -- new block
+    local x = x           -- new 'x', with value 10
+    print(x)              --> 10
+    x = x+1
+    do                    -- another block
+        local x = x+1     -- another 'x'
+        print(x)          --> 12
+    end
+    print(x)              --> 11
+end
+print(x)                  --> 10  (the global one)
 """
 
     print("\n".join([f"> {line}" for line in text[1:].splitlines()]))
