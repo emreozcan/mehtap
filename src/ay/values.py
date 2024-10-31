bb7f0b113770178c4bde54087d35477d798f55d3
Emre Özcan <emre@emreis.com>
2024-11-01 02:45:33 +0300

    Add docstrings to values.py

--- src/ay/values.py
+++ src/ay/values.py
@@ -14,38 +14,58 @@ if TYPE_CHECKING:
 
 @attrs.define(slots=True, eq=False)
 class LuaValue(ABC):
+    """Base class that all Lua values inherit from."""
+
     def get_metatable(self) -> LuaNilType | LuaTable:
+        """
+        :return: The value's metatable if it exists, or :data:`LuaNil` if not.
+        """
         if hasattr(self, "_metatable"):
             return self._metatable
         return LuaNil
 
     def has_metamethod(self, name: LuaString) -> bool:
+        """
+        :param name: The name of the metavalue to check for.
+        :return: Whether the value has a metavalue with this key.
+        """
         metatable = self.get_metatable()
         if metatable is LuaNil:
             return False
         return metatable.has(name)
 
     def get_metamethod(self, name: LuaString) -> LuaValue | None:
+        """
+        :param name: The name of the metavalue to get.
+        :return: The metavalue, or :data:`None` if the value doesn't have a
+                 metavalue with the given name.
+        """
         metatable = self.get_metatable()
         if metatable is LuaNil:
             return None
         return metatable.get_with_fallback(name, fallback=None)
 
     def set_metatable(self, value: LuaTable):
+        """Set the value's metatable if the value can have one.
+
+        :raises NotImplementedError: if the value can't have a metatable
+        """
         if "_metatable" in self.__slots__:
             self._metatable = value
         else:
             raise NotImplementedError()
 
-    def __eq__(self, other) -> bool:
+    def __eq__(self, other: LuaValue) -> bool:
+        """Compare this value according to Lua's rules on equality."""
         from .operations import rel_eq
 
         return rel_eq(self, other).true
 
-    def __ne__(self, other) -> bool:
+    def __ne__(self, other: LuaValue) -> bool:
         from .operations import rel_ne
 
         return rel_ne(self, other).true
+    __ne__.__doc__ = __eq__.__doc__
 
 
 @attrs.define(slots=True, eq=False, repr=False)
@@ -61,12 +81,21 @@ class LuaNilType(LuaValue):
 
 
 LuaNil = LuaNilType()
-del LuaNilType
+"""Value that is different from all other LuaValues.
+
+Represents the value of the *nil* base type in Lua.
+Sole object of the `LuaNilType` class.
+"""
+if not TYPE_CHECKING:
+    del LuaNilType
 
 
 @attrs.define(slots=True, eq=False)
 class LuaBool(LuaValue):
+    """Class representing values of the *boolean* base type in Lua."""
+
     true: bool
+    """Whether this value is `true` or `false`."""
 
     def __str__(self) -> str:
         return "true" if self.true else "false"
@@ -76,8 +105,11 @@ class LuaBool(LuaValue):
 
 
 class LuaNumberType(Enum):
+    """Enumeration of the types of numbers in Lua."""
     INTEGER = 1
+    """Number type that represents 64-bit signed integers."""
     FLOAT = 2
+    """Number type that represents double-precision floating point numbers."""
 
 
 MAX_INT64 = 2**63 - 1
@@ -88,14 +120,27 @@ ALL_SET = 2**64 - 1
 
 @attrs.define(slots=True, init=False, eq=False)
 class LuaNumber(LuaValue):
+    """Class representing values of the *number* base type in Lua.
+
+    .. automethod:: LuaNumber.__init__
+    """
+
     value: int | float
+    """The underlying value of this number value."""
     type: LuaNumberType | None
+    """The type of this number value."""
 
     def __init__(
         self,
         value: int | float,
         type: LuaNumberType | None = None,
     ) -> None:
+        """Initialize a new LuaNumber object.
+
+        :param value: The underlying value of this number value.
+        :param type: The type of this number value.
+                     If provided, must match the type of `value`.
+        """
         super().__init__()
         self.value = value
         if type is None:
@@ -119,7 +164,10 @@ class LuaNumber(LuaValue):
 
 @attrs.define(slots=True, eq=False, frozen=True)
 class LuaString(LuaValue):
+    """Class representing values of the *string* base type in Lua."""
+
     content: bytes
+    """Sequence of the bytes of the string."""
 
     def __str__(self) -> str:
         return self.content.decode("utf-8")
@@ -130,40 +178,52 @@ class LuaString(LuaValue):
 
 @attrs.define(slots=True, eq=False)
 class LuaObject(LuaValue, ABC):
+    """Base class that *objects* inherit from.
+
+    :class:`Tables <LuaTable>`,
+    :class:`functions <LuaFunction>`,
+    :class:`threads <LuaThread>` and
+    :class:`(full) userdata values <LuaUserdata>`
+    are objects.
+
+    `Reference Manual 2.1`_.
+
+    .. _Reference Manual 2.1: https://lua.org/manual/5.4/manual.html#2.1
+    """
+
     def __str__(self):
         return repr(self)
 
 
 @attrs.define(slots=True, eq=False)
 class LuaUserdata(LuaObject):
+    """Class representing values of the *userdata* base type in Lua."""
     pass
 
 
 @attrs.define(slots=True, eq=False)
 class LuaThread(LuaObject):
+    """Class representing values of the *thread* base type in Lua."""
     pass
 
 
 @attrs.define(slots=True, eq=False, repr=False)
 class LuaTable(LuaObject):
+    """Class representing values of the *table* base type in Lua."""
     map: dict[LuaValue, LuaValue] = attrs.field(factory=dict)
+    """The key-value pairs of the table."""
     _metatable: LuaTable = LuaNil
+    """The metatable of the table."""
 
     def __repr__(self):
         if not self._metatable:
             return f"<LuaTable {self!s}>"
         return f"<LuaTable {self!s} metatable={self._metatable}>"
 
-    def get_metatable(self):
-        return self._metatable
-
-    def set_metatable(self, value: "LuaValue"):
-        self._metatable = value
-
     def __str__(self):
-        return self.recursive_detecting_str(set())
+        return self._recursive_detecting_str(set())
 
-    def recursive_detecting_str(
+    def _recursive_detecting_str(
         self,
         seen_objects: set[int],
     ) -> str:
@@ -176,16 +236,20 @@ class LuaTable(LuaObject):
             if not isinstance(key, LuaTable):
                 key_str = str(key)
             else:
-                key_str = key.recursive_detecting_str(seen_objects)
+                key_str = key._recursive_detecting_str(seen_objects)
             if not isinstance(value, LuaTable):
                 value_str = str(value)
             else:
-                value_str = value.recursive_detecting_str(seen_objects)
+                value_str = value._recursive_detecting_str(seen_objects)
             pair_list.append((key_str, value_str))
         return "{" + ", ".join(f"({k})=({v})" for k, v in pair_list) + "}"
 
     # TODO: Change raw's default value to False.
     def put(self, key: LuaValue, value: LuaValue, *, raw: bool = True):
+        """Put a key-value pair into the table.
+
+        :param raw: Whether to follow the rules of `rawset()`.
+        """
         if not raw:
             raise NotImplementedError()  # todo. (__newindex metavalue)
 
@@ -205,6 +269,12 @@ class LuaTable(LuaObject):
 
     # TODO: Change raw's default value to False.
     def get(self, key: LuaValue, *, raw: bool = True) -> LuaValue:
+        """Get the associated value of a key from the table.
+
+        :param raw: Whether to follow the rules of `rawget()`.
+        :returns: The value associated with the key, or :data:`LuaNil`.
+        """
+
         if not raw:
             raise NotImplementedError()  # todo. (__index metavalue)
         if key in self.map:
@@ -218,29 +288,66 @@ class LuaTable(LuaObject):
         return key in self.map
 
 
-@attrs.define(slots=True, eq=True)
+@attrs.define(slots=True, eq=True, repr=False)
 class Variable:
+    """Tuple of a Lua value and its properties describing a local variable."""
     value: LuaValue
+    """The value of the variable."""
     constant: bool = False
+    """Whether the local is a constant."""
     to_be_closed: bool = False
+    """Whether the local is a to-be-closed variable."""
 
     def __repr__(self):
         if self.constant:
-            return f"<const {self.value}>"
+            return f"<Variable const {self.value}>"
         if self.to_be_closed:
-            return f"<close {self.value}>"
-        return f"<var {self.value}>"
+            return f"<Variable close {self.value}>"
+        return f"<Variable var {self.value}>"
 
 
 @attrs.define(slots=True, eq=False)
 class LuaFunction(LuaObject):
+    """Class representing values of the *function* base type in Lua."""
     param_names: list[LuaString] | None
+    """The names of the parameters of the function.
+
+    Used when binding given arguments to the block scope when calling the
+    function or when pretty-displaying the function.
+    """
     variadic: bool
+    """Whether the function is a *variadic function*.
+
+    That is, a function that accepts a variable number of arguments, binding
+    the excess arguments to the expression "`...`".
+    """
     parent_scope: Scope | None
+    """The scope the function was defined in.
+
+    The function can access any of these values as *upvalues*.
+    Not applicable for functions defined from Python.
+    """
     block: Block | Callable
+    """The code that the function executes.
+
+    If of type :class:`Block`, the function is implemented in Lua.
+    If of type :class:`Callable`, the function is implemented in Python.
+
+    The :meth:`call` method should be used to call the function.
+    """
     gets_scope: bool = False
+    """Whether the function receives the scope as its first argument.
+
+    Only applicable for functions implemented in Python.
+    """
     name: str | None = None
+    """The name of the function."""
     min_req: int | None = None
+    """The minimum number of required arguments.
+
+    Only applicable for functions implemented in Python.
+    Only used for pretty-displaying the function.
+    """
 
     def _py_param_str(self, index):
         if (
@@ -282,11 +389,28 @@ class LuaFunction(LuaObject):
     def call(
         self,
         args: list[LuaValue | list[LuaValue]],
-        scope: Scope,
+        scope: Scope | None,
     ) -> list[LuaValue]:
+        """Call the function.
+
+        :param args: The arguments to pass to the function.
+                     The number of arguments will be suitably adjusted to the
+                     number of the function's arguments according to
+                     `the rules on adjustment of Lua`_.
+                     Can contain multires expressions.
+        :param scope: The scope of the caller.
+        :return: A multires list of the return values of the function.
+
+        .. _the rules on adjustment of Lua:
+           https://lua.org/manual/5.4/manual.html#3.4.12
+        """
         from ay.control_structures import ReturnException
+        from ay.vm import VirtualMachine
         try:
-            self._call(args, scope or self.parent_scope)
+            self._call(
+                args,
+                scope or self.parent_scope or VirtualMachine().root_scope,
+            )
         except ReturnException as e:
             return e.values if e.values is not None else []
         return []
