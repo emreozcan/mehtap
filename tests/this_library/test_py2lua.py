64fcae379c5459127096de860a01736e7a760866
Emre Özcan <emre@emreis.com>
2024-11-01 02:46:19 +0300

    Add tests for py2lua

---
+++ tests/this_library/test_py2lua.py
@@ -0,0 +1,66 @@
+from ay.py2lua import py2lua, PyLuaRet
+from ay.values import LuaBool, LuaNil, LuaNumber, LuaNumberType, LuaString
+
+
+def test_booleans():
+    assert py2lua(True) == LuaBool(True)
+    assert py2lua(False) == LuaBool(False)
+
+
+def test_nil():
+    assert py2lua(None) == LuaNil
+
+
+def test_numbers():
+    _42 = py2lua(42)
+    assert _42 == LuaNumber(42)
+    assert _42.type == LuaNumberType.INTEGER
+
+    _42_0 = py2lua(42.0)
+    assert _42_0 == LuaNumber(42.0)
+    assert _42_0.type == LuaNumberType.FLOAT
+
+
+def test_strings():
+    assert py2lua("hello") == LuaString(b"hello")
+
+
+def test_mappings():
+    country_codes = {"US": "United States", "CA": "Canada"}
+
+    lua_table = py2lua(country_codes)
+
+    assert lua_table.get(LuaString(b"US")) == LuaString(b"United States")
+    assert lua_table.get(LuaString(b"CA")) == LuaString(b"Canada")
+
+
+def test_iterables():
+    numbers = [200, 500, 900]
+
+    lua_table = py2lua(numbers)
+
+    assert lua_table.get(LuaNumber(1)) == LuaNumber(200)
+    assert lua_table.get(LuaNumber(2)) == LuaNumber(500)
+    assert lua_table.get(LuaNumber(3)) == LuaNumber(900)
+
+
+def test_callables():
+    def add(a, b, /):
+        assert isinstance(a, int)
+        assert isinstance(b, int)
+        return a + b
+
+    lua_function = py2lua(add)
+
+    assert lua_function.call(
+        [LuaNumber(1), LuaNumber(2)],
+        None
+    ) == [LuaNumber(3)]
+    assert lua_function.call(
+        [LuaNumber(3), LuaNumber(4)],
+        None
+    ) == [LuaNumber(7)]
+    assert lua_function.call(
+        [LuaNumber(5), LuaNumber(6)],
+        None
+    ) == [LuaNumber(11)]
