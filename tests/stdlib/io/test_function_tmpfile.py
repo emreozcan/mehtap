abac03b71b0ceca747594542ae722db84ed59000
Emre Özcan <emre@emreis.com>
2024-11-03 18:38:57 +0300

    Fix a test failing due on POSIX by using portable method

--- tests/stdlib/io/test_function_tmpfile.py
+++ tests/stdlib/io/test_function_tmpfile.py
@@ -1,3 +1,4 @@
+import os.path
 from pathlib import Path
 
 from ay.values import LuaString
@@ -13,4 +14,4 @@ def test_tmpfile():
     vm.exec("s = fh:read('a')")
     assert vm.get_ls(LuaString(b"s")) == LuaString(b"abc\ndef\nghi\n")
     vm.exec("fh:close()")
-    assert not Path(fh.io.name).exists()
+    assert not os.path.exists(fh.io.name)
