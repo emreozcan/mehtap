ea632b496d63fae6d5888cb0f6933cdbdc6903f6
Emre Özcan <emre@emreis.com>
2024-11-03 05:11:08 +0300

    Add os.exit(boolean) tests

--- tests/stdlib/os/test_exit.py
+++ tests/stdlib/os/test_exit.py
@@ -5,11 +5,25 @@ from ay.vm import VirtualMachine
 vm = VirtualMachine()
 
 
-def test_exit_true():
+def test_exit_42():
     with pytest.raises(SystemExit) as system_exit:
         vm.exec("os.exit(42, true)")
     assert system_exit.type == SystemExit
     assert system_exit.value.code == 42
+
+
+def test_exit_bool_true():
+    with pytest.raises(SystemExit) as system_exit:
+        vm.exec("os.exit(true, true)")
+    assert system_exit.type == SystemExit
+    assert system_exit.value.code == 0
+
+
+def test_exit_bool_false():
+    with pytest.raises(SystemExit) as system_exit:
+        vm.exec("os.exit(false, true)")
+    assert system_exit.type == SystemExit
+    assert system_exit.value.code == 1
 
 
 @pytest.mark.skip(reason="No way to test this")
